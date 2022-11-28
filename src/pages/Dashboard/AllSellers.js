f8ce925cd7dfb0c21f8ed700da37c3a8776db46c
ifty17 <iftyrahatkabir@gmail.com>
2022-11-28 16:48:42 +0600

    added delete seller

--- src/pages/Dashboard/AllSellers.js
+++ src/pages/Dashboard/AllSellers.js
@@ -1,17 +1,30 @@
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import toast from 'react-hot-toast';
 
 const AllSellers = () => {
 
-    const {data: allsellers = []} = useQuery({
+    const {data: allsellers = [], refetch} = useQuery({
         queryKey: ['allseller'],
         queryFn: async () =>{
             const res = await fetch("http://localhost:5000/allsellers");
             const data = await res.json();
             return data;
         }
-    })
-    console.log(allsellers);
+    });
+
+    const handleDeleteSeller = (seller) => {
+      fetch(`http://localhost:5000/seller/${seller._id}`, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.deletedCount) {
+            refetch();
+            toast.success(`Seller ${seller?.displayName} deleted successful`);
+          }
+        });
+    };
 
     return (
       <div className="overflow-x-auto">
@@ -29,10 +42,10 @@ const AllSellers = () => {
             {allsellers.map((allseller) => (
               <tr key={allseller._id}>
                 <th>1</th>
-                <td><img className='w-16 rounded-full' src={allseller.image} alt="" /></td>
-                <td>{allseller.displayName}</td>
-                <td>{allseller.email}</td>
-                <td><button className='btn btn-xs btn-outline'>Delete Seller</button></td>
+                <td><img className='w-16 rounded-full' src={allseller?.image} alt="" /></td>
+                <td>{allseller?.displayName}</td>
+                <td>{allseller?.email}</td>
+                <td><button onClick={() => handleDeleteSeller(allseller) } className='btn btn-xs btn-outline'>Delete Seller</button></td>
               </tr>
             ))}
           </tbody>
