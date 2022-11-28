e38c0ac14d8aa45b5134fc12884d6a3ffefa80de
ifty17 <iftyrahatkabir@gmail.com>
2022-11-28 22:04:29 +0600

    design fixed

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -4,7 +4,7 @@ const Dashboard = () => {
     return (
       <div className="text-center pt-12">
         <h1 className="text-6xl pb-3  font-bold">Welcome To Dashboard!</h1>
-        <p>Here you can see all your activities you did in this website. For more see the navigation bar in left side.</p>
+        <p>Here you can see all your activities you did in this website. For more see the navigation bar.</p>
       </div>
     );
 };
