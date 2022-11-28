749509e69a639b1fad3a0dc07f85c2a2f9cf7a27
ifty17 <iftyrahatkabir@gmail.com>
2022-11-28 09:15:17 +0600

    added all seller

--- src/pages/Dashboard/AllSellers.js
+++ src/pages/Dashboard/AllSellers.js
@@ -1,10 +1,41 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 
 const AllSellers = () => {
+
+    const {data: allsellers = []} = useQuery({
+        queryKey: ['allseller'],
+        queryFn: async () =>{
+            const res = await fetch("http://localhost:5000/allsellers");
+            const data = await res.json();
+            return data;
+        }
+    })
+    console.log(allsellers);
+
     return (
-        <div>
-            
-        </div>
+      <div className="overflow-x-auto">
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Seller Name</th>
+              <th>Seller Email</th>
+              <th>Delete Seller</th>
+            </tr>
+          </thead>
+          <tbody>
+            {allsellers.map((allseller) => (
+              <tr key={allseller._id}>
+                <th>1</th>
+                <td>{allseller.displayName}</td>
+                <td>{allseller.email}</td>
+                <td><button className='btn btn-xs btn-outline'>Delete Seller</button></td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     );
 };
 
