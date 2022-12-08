f02aa5012536d8475c1fef2c2fd0b7032c9a59b1
ifty17 <iftyrahatkabir@gmail.com>
2022-12-08 17:00:15 +0600

    added carousel

--- src/pages/Home/Slider/Slider.js
+++ src/pages/Home/Slider/Slider.js
@@ -6,7 +6,7 @@ import slider3 from '../../../assets/sliderimg2.jpg'
 const Slider = () => {
     return (
       <div data-aos="zoom-in-up">
-        <div className="carousel w-full">
+        {/* <div className="carousel w-full">
           <div id="item1" className="carousel-item w-full">
             <img
               src={slider1}
@@ -39,6 +39,53 @@ const Slider = () => {
           <a href="#item3" className="btn btn-xs">
             3
           </a>
+        </div> */}
+        <div className="carousel w-full">
+          <div id="slide1" className="carousel-item relative w-full">
+            <img
+              src={slider1}
+              alt="/"
+              className="w-full h-[250px] md:h-[750px]"
+            />
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+              <a href="#slide4" className="btn btn-circle btn-outline">
+                ❮
+              </a>
+              <a href="#slide2" className="btn btn-circle btn-outline">
+                ❯
+              </a>
+            </div>
+          </div>
+          <div id="slide2" className="carousel-item relative w-full">
+            <img
+              src={slider2}
+              alt="/"
+              className="w-full h-[250px] md:h-[750px]"
+            />
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+              <a href="#slide1" className="btn btn-circle btn-outline">
+                ❮
+              </a>
+              <a href="#slide3" className="btn btn-circle btn-outline">
+                ❯
+              </a>
+            </div>
+          </div>
+          <div id="slide3" className="carousel-item relative w-full">
+            <img
+              src={slider3}
+              alt="/"
+              className="w-full h-[250px] md:h-[750px]"
+            />
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+              <a href="#slide2" className="btn btn-circle btn-outline">
+                ❮
+              </a>
+              <a href="#slide4" className="btn btn-circle btn-outline">
+                ❯
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     );
